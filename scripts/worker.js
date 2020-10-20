b127aa5a1ca9f94817d3fa47565d2a85a253ebd6
lushipeng <45868383+lushipeng@users.noreply.github.com>
2020-10-20 17:56:23 +0800

    update

--- scripts/worker.js
+++ scripts/worker.js
@@ -1,6 +1,6 @@
 onmessage = function() {
     let myDate;
-    for (let i = 0; i < 1000000; i++) {
+    for (let i = 0; i < 10000000; i++) {
         let date = new Date();
         myDate = date;
     }
